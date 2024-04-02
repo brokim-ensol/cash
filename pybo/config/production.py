d36247a62781bd9c5d7bcf79b5362aa2f096b229
brokim <brokim@lgensol.com>
2024-04-02 20:43:27 +0900

    프로덕션 설정 파일 삭제***
    ***프로덕션 설정 파일을 삭제했습니다

--- pybo/config/production.py
+++
@@ -1,5 +0,0 @@
-from pybo.config.default import *
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = b'\x18\x93\xdb\x92|\tG\x8dZo \x0b\x89\xa7L\xba'
