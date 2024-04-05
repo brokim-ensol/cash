bffa9e36fa2dc0a9db1139a40aadffbca1a9709c
hkim <mfiacp@hanmail.net>
2024-04-06 00:41:51 +0900

    모델 및 뷰 수정

--- pybo/models.py
+++ pybo/models.py
@@ -1,21 +1,26 @@
 from pybo import db
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import Mapped
 
 db: SQLAlchemy
+
 
 class Repayment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     category = db.Column(db.String(200), nullable=False)
     amount = db.Column(db.Integer, nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False)
-    modified_at = db.Column(db.DateTime, nullable=True)
+    created_at = db.Column(db.Date, nullable=False)
+    modified_at = db.Column(db.Date, nullable=True)
     remark = db.Column(db.String(200), nullable=True)
-    
+
+
 class Balance(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    repayment_id = db.Column(db.Integer, db.ForeignKey('repayment.id', ondelete='CASCADE'))
-    repayment = db.relationship('Repayment', backref=db.backref('balance_set', cascade='all, delete-orphan'))
-    amount = db.Column(db.Integer, nullable=False)
+    repayment_id = db.Column(db.Integer, db.ForeignKey("repayment.id"))
+    repayment: Mapped["Repayment"] = db.relationship(
+        "Repayment", backref=db.backref("balance", uselist=False)
+    )
     balance = db.Column(db.Integer, nullable=True)
     category = db.Column(db.String(200), nullable=True)
-    repaid_dt = db.Column(db.DateTime, nullable=False)
+    repaid_dt = db.Column(db.Date, nullable=False)
+    ratio = db.Column(db.Float, nullable=True)
--- pybo/views/repayment_views.py
+++ pybo/views/repayment_views.py
@@ -4,13 +4,14 @@ from pybo.models import Repayment, Balance
 from pybo import db
 from pybo.forms import RepaymentFoam
 import pandas as pd
+from sqlalchemy import update
 
 bp = Blueprint("repayment", __name__, url_prefix="/repayment")
 
 
 @bp.route("/list/")
 def _list():
-    page = request.args.get('page', type=int, default=1)  # 페이지
+    page = request.args.get("page", type=int, default=1)  # 페이지
     repayment_list = Repayment.query.order_by(Repayment.created_at.desc())
     repayment_list = repayment_list.paginate(page=page, per_page=10)
     return render_template(
@@ -29,7 +30,7 @@ def create():
     form = RepaymentFoam()
     if request.method == "POST" and form.validate_on_submit():
         created_at = form.created_at.data
-        remark = form.remark.data if form.remark.data else ''
+        remark = form.remark.data if form.remark.data else ""
         repayment = Repayment(
             category=form.category.data,
             amount=form.amount.data,
@@ -39,40 +40,60 @@ def create():
         db.session.add(repayment)
         balance = Balance(
             repayment_id=repayment.id,
-            amount=form.amount.data,
             repaid_dt=created_at,
             category=form.category.data,
         )
-        repayment.balance_set.append(balance)
-        balance_list = Balance.query.all()
-        #unpacking balance_list which is a list of Balance objects
-        balance_data = [(balance.id, balance.repayment_id, balance.amount, balance.repaid_dt, balance.category) for balance in balance_list]
-        balance_df = pd.DataFrame.from_records(balance_data, columns=["id", "repayment_id", "amount", "repaid_dt", "category"])
-        balance_df = balance_df.sort_values(by=['repaid_dt', 'id'], ascending=[True, True])
-        balance_df['balance'] = 32000 - balance_df['amount'].cumsum()
-        balance_df['ratio'] = balance_df['amount'].cumsum() / 32000
-        balance_df.to_sql('balance', db.engine, if_exists='replace', index=False, index_label='id')
-        db.session.commit()
+        repayment.balance = balance
+        update_balance()
         return render_template("repayment/repayment_detail.html", repayment=repayment)
     return render_template("repayment/repayment_create.html", form=form)
 
-@bp.route('/modify/<int:repayment_id>', methods=('GET', 'POST'))
+
+@bp.route("/modify/<int:repayment_id>", methods=("GET", "POST"))
 def modify(repayment_id):
     repayment = Repayment.query.get_or_404(repayment_id)
-    if request.method == 'POST':  # POST 요청
+    if request.method == "POST":  # POST 요청
         form = RepaymentFoam()
         if form.validate_on_submit():
             form.populate_obj(repayment)
-            repayment.modified_at = datetime.now()  # 수정일시 저장
+            repayment.modified_at = datetime.now().date()
+            update_balance()
             db.session.commit()
-            return redirect(url_for('repayment.detail', repayment_id=repayment_id))
+            return redirect(url_for("repayment.detail", repayment_id=repayment_id))
     else:  # GET 요청
         form = RepaymentFoam(obj=repayment)
-    return render_template('repayment/repayment_create.html', form=form)
+    return render_template("repayment/repayment_create.html", form=form)
 
-@bp.route('/delete/<int:repayment_id>')
+
+@bp.route("/delete/<int:repayment_id>")
 def delete(repayment_id):
     repayment = Repayment.query.get_or_404(repayment_id)
     db.session.delete(repayment)
     db.session.commit()
-    return redirect(url_for('repayment._list'))
+    return redirect(url_for("repayment._list"))
+
+
+def update_balance():
+    balance_list = Balance.query.all()
+    # unpacking balance_list which is a list of Balance objects
+    balance_data = [
+        (
+            balance.id,
+            balance.repayment_id,
+            balance.repayment.amount,
+            balance.repaid_dt,
+            balance.category,
+        )
+        for balance in balance_list
+    ]
+    balance_df = pd.DataFrame.from_records(
+        balance_data,
+        columns=["id", "repayment_id", "amount", "repaid_dt", "category"],
+    )
+    balance_df = balance_df.sort_values(by=["repaid_dt", "id"], ascending=[True, True])
+    balance_df["balance"] = 32000 - balance_df["amount"].cumsum()
+    balance_df["ratio"] = 1 - (balance_df["amount"].cumsum() / 32000)
+    balance_df.drop(columns="amount", inplace=True)
+    balance_list = balance_df.to_dict(orient="records")
+    db.session.bulk_update_mappings(Balance, balance_list)
+    db.session.commit()
