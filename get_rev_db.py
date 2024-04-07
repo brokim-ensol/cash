caa00069faeb8cf73984e3b979aff79b92818f42
brokim <brokim@lgensol.com>
2024-04-07 19:55:34 +0900

    get_rev_db 함수 수정

--- get_rev_db.py
+++ get_rev_db.py
@@ -1,6 +1,4 @@
-#sqlite3를 이용하여 현재 같은 폴더에 있는 pybo.db 파일을 읽는다.
-#그리고 alembic_version 테이블의 version_num을 조회한다.
-#이렇게 조회한 version_num을 리턴한다.
+#!./venv python
 def get_rev_db():
     import sqlite3
     try:
@@ -9,9 +7,9 @@ def get_rev_db():
         cur.execute('SELECT version_num FROM alembic_version')
         version_num = cur.fetchone()
         conn.close()
-        return version_num[0]
+        print(version_num[0],end=None)
     except:
-        return "0"
+        print("0",end=None)
 
 if __name__ == '__main__':
-    version_number = get_rev_db()
+    get_rev_db()
